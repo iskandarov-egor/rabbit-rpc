3459da617cc35c4cfa8c4db1873e5f75d551ce43
iskandarov-egor <iskandarov_egor@mail.ru>
2016-04-06 20:35:03 +0300

    __init__ fix

--- rabbit_rpc/__init__.py
+++ rabbit_rpc/__init__.py
@@ -1,4 +1,4 @@
-from exceptions import BadResponseException, RabbitRpcException
-from rabbit_rpc_server import RabbitRpcServer
-from request import RabbitRpcRequest
-from rpc_raw import rpc_request_raw
+from rabbit_rpc.exceptions import BadResponseException, RabbitRpcException
+from rabbit_rpc.rabbit_rpc_server import RabbitRpcServer
+from rabbit_rpc.request import RabbitRpcRequest
+from rabbit_rpc.rpc_raw import rpc_request_raw
